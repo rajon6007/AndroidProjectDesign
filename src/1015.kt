64d4a04fd1263e2e89cd92ca025654d968185104
rajon6007 <paulrajon412@gmail.com>
2024-09-05 18:25:55 +0600

    done

--- src/1015.kt
+++ src/1015.kt
@@ -1,12 +1,15 @@
 import kotlin.math.sqrt
+import java.util.*
 
 fun main(args: Array<String>) {
-    val x1 = readLine()!!.toDouble()
-    val y1 = readLine()!!.toDouble()
+    val a =Scanner(System.`in`)
 
-    val x2 = readLine()!!.toDouble()
-    val y2 = readLine()!!.toDouble()
+    val x1 = a.nextDouble()
+    val y1 = a.nextDouble()
+
+    val x2 = a.nextDouble()
+    val y2 = a.nextDouble()
 
     val Distance = sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)))
-    println("$Distance %.4f".format())
+    println("%.4f".format(Distance))
 }
